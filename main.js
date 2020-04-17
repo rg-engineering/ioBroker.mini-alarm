b6297eca4d07627511fae7d04a20970e491cb743
Rene Glaß <info@rg-engineering.eu>
2020-04-17 21:53:59 +0200

    admin plus arm of sensors

--- main.js
+++ main.js
@@ -1,156 +1,370 @@
+/*
+ * mini-alarm adapter für iobroker
+ *
+ * Created: 03.04.2020 21:31:28
+ *  Author: Rene
+
+*/
+
+
+
+
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
 "use strict";
 
-/*
- * Created with @iobroker/create-adapter v1.21.0
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
+// you have to require the utils module and call adapter function
 const utils = require("@iobroker/adapter-core");
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+let adapter;
 
-class MiniAlarm extends utils.Adapter {
+/** @type {number | null } */
+let ArmTimer = null;
 
-    /**
-     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
-     */
-    constructor(options) {
-        super({
-            ...options,
-            name: "mini-alarm",
-        });
-        this.on("ready", this.onReady.bind(this));
-        this.on("objectChange", this.onObjectChange.bind(this));
-        this.on("stateChange", this.onStateChange.bind(this));
-        // this.on("message", this.onMessage.bind(this));
-        this.on("unload", this.onUnload.bind(this));
-    }
+function startAdapter(options) {
+    options = options || {};
+    Object.assign(options, {
+        name: "mini-alarm",
+        //#######################################
+        //
+        ready: function () {
+            try {
+                //adapter.log.debug("start");
+                main();
+            }
+            catch (e) {
+                adapter.log.error("exception catch after ready [" + e + "]");
+            }
+        },
+        //#######################################
+        //  is called when adapter shuts down
+        unload: function (callback) {
+            try {
+                adapter && adapter.log && adapter.log.info && adapter.log.info("cleaned everything up...");
 
-    /**
-     * Is called when databases are connected and adapter received configuration.
-     */
-    async onReady() {
-        // Initialize your adapter here
+                callback();
+            } catch (e) {
+                callback();
+            }
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info("config option1: " + this.config.option1);
-        this.log.info("config option2: " + this.config.option2);
 
+
+        },
+        //#######################################
+        //
+        SIGINT: function () {
+            adapter && adapter.log && adapter.log.info && adapter.log.info("cleaned everything up...");
+
+        },
+        //#######################################
+        //  is called if a subscribed object changes
+        //objectChange: function (id, obj) {
+        //    adapter.log.debug("[OBJECT CHANGE] ==== " + id + " === " + JSON.stringify(obj));
+        //},
+        //#######################################
+        // is called if a subscribed state changes
+        stateChange: function (id, state) {
+            //adapter.log.debug("[STATE CHANGE] ==== " + id + " === " + JSON.stringify(state));
+            HandleStateChange(id, state);
+        },
+        //#######################################
+        //
         /*
-        For every state in the system there has to be also an object of type state
-        Here a simple template for a boolean variable named "testVariable"
-        Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
+        message: async (obj) => {
+            if (obj) {
+                switch (obj.command) {
+
+                    default:
+                        adapter.log.error("unknown message " + obj.command);
+                        break;
+                }
+            }
+        }
         */
-        await this.setObjectAsync("testVariable", {
-            type: "state",
-            common: {
-                name: "testVariable",
-                type: "boolean",
-                role: "indicator",
-                read: true,
-                write: true,
-            },
-            native: {},
-        });
+    });
+    adapter = new utils.Adapter(options);
 
-        // in this template all states changes inside the adapters namespace are subscribed
-        this.subscribeStates("*");
+    return adapter;
+}
+        
 
-        /*
-        setState examples
-        you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync("testVariable", true);
 
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync("testVariable", { val: true, ack: true });
+//#######################################
+//
+async function main() {
+    try {
+        adapter.log.debug("sensors " + JSON.stringify(adapter.config.sensors));
+        adapter.log.debug("actors " + JSON.stringify(adapter.config.actors));
 
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+        await CreateDatepoints();
 
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync("admin", "iobroker");
-        this.log.info("check user admin pw iobroker: " + result);
+        await SubscribeStates();
 
-        result = await this.checkGroupAsync("admin", "admin");
-        this.log.info("check group user admin group admin: " + result);
     }
-
-    /**
-     * Is called when adapter shuts down - callback has to be called under any circumstances!
-     * @param {() => void} callback
-     */
-    onUnload(callback) {
-        try {
-            this.log.info("cleaned everything up...");
-            callback();
-        } catch (e) {
-            callback();
-        }
+    catch (e) {
+        adapter.log.error("exception in  main [" + e + "]");
     }
-
-    /**
-     * Is called if a subscribed object changes
-     * @param {string} id
-     * @param {ioBroker.Object | null | undefined} obj
-     */
-    onObjectChange(id, obj) {
-        if (obj) {
-            // The object was changed
-            this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-        } else {
-            // The object was deleted
-            this.log.info(`object ${id} deleted`);
-        }
-    }
-
-    /**
-     * Is called if a subscribed state changes
-     * @param {string} id
-     * @param {ioBroker.State | null | undefined} state
-     */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
-            // The state was deleted
-            this.log.info(`state ${id} deleted`);
-        }
-    }
-
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.message" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    // 	if (typeof obj === "object" && obj.message) {
-    // 		if (obj.command === "send") {
-    // 			// e.g. send email or pushover or whatever
-    // 			this.log.info("send command");
-
-    // 			// Send response in callback if required
-    // 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-    // 		}
-    // 	}
-    // }
 
 }
 
-// @ts-ignore parent is a valid property on module
-if (module.parent) {
-    // Export the constructor in compact mode
-    /**
-     * @param {Partial<ioBroker.AdapterOptions>} [options={}]
-     */
-    module.exports = (options) => new MiniAlarm(options);
+//#######################################
+//
+// create all necessary datapaoints
+// will be called at ecery start of adapter
+async function CreateDatepoints() {
+
+    adapter.log.debug("start CreateDatepoints");
+
+
+    try {
+        await adapter.setObjectNotExistsAsync("State", {
+            type: "state",
+            common: {
+                name: "State",
+                role: "value",
+                type: "string",
+                unit: "",
+                read: true,
+                write: false
+            },
+            native: { id: "State" }
+        });
+
+        await adapter.setStateAsync("State", { ack: true, val: "disarmed" });
+
+        await adapter.setObjectNotExistsAsync("ArmAll", {
+            type: "state",
+            common: {
+                name: "ArmAll",
+                role: "button",
+                type: "boolean",
+                unit: "",
+                read: false,
+                write: true
+            },
+            native: { id: "ArmAll" }
+        });
+
+        await adapter.setObjectNotExistsAsync("Disarm", {
+            type: "state",
+            common: {
+                name: "Disarm",
+                role: "button",
+                type: "boolean",
+                unit: "",
+                read: false,
+                write: true
+            },
+            native: { id: "Disarm" }
+        });
+
+        for (let i = 1; i <= adapter.config.NumberOfZones; i++) {
+            await adapter.setObjectNotExistsAsync("ArmZone"+i, {
+                type: "state",
+                common: {
+                    name: "ArmAZone"+i,
+                    role: "button",
+                    type: "boolean",
+                    unit: "",
+                    read: false,
+                    write: true
+                },
+                native: { id: "ArmZone"+i }
+            });
+        }
+
+    }
+    catch (e) {
+        adapter.log.error("exception in  CreateDatepoints [" + e + "]");
+    }
+
+}
+
+//#######################################
+//
+// subscribe thermostate states to be informed when target or current is changed
+/**
+ * @param {(() => void) | undefined} [callback]
+ */
+function SubscribeStates(callback) {
+
+    //if we need to handle actors, then subscribe on current and target temperature
+    adapter.log.debug("#start subscribtion ");
+
+    try {
+        adapter.subscribeStates("ArmAll");
+        adapter.subscribeStates("Disarm");
+
+        for (let i = 1; i <= adapter.config.NumberOfZones; i++) {
+            adapter.subscribeStates("ArmZone"+i);
+        }
+
+
+
+        adapter.log.debug("#subscribtion finished");
+    }
+    catch (e) {
+        adapter.log.error("exception in SubscribeStates [" + e + "]");
+    }
+    if (callback) callback();
+}
+
+//*******************************************************************
+//
+// handles state changes of subscribed states
+
+let LastStateChangeID = "";
+let LastStateVal = 1;
+
+/**
+ * @param {string} id
+ * @param {{ ack: boolean; val: string | number; }} state
+ */
+async function HandleStateChange(id, state) {
+
+    adapter.log.debug("### handle state change " + id + " " + JSON.stringify(state));
+
+    try {
+
+        if (state && state.ack !== true) {
+            //first set ack flag
+            await adapter.setStateAsync(id, { ack: true });
+        }
+
+        if (id !== LastStateChangeID || state.val !== LastStateVal) {
+
+            adapter.log.debug("### " + id + " " + LastStateChangeID + " " + state.val + " " + LastStateVal);
+
+
+            let bHandled = false;
+            LastStateChangeID = id;
+            LastStateVal = state.val;
+
+
+            const ids = id.split("."); 
+
+
+            if (ids[2] === "Disarm") {
+                await Disarm();
+                bHandled = true;
+            }
+            else if (ids[2] === "ArmAll") {
+                await Arm(-1);
+                bHandled = true;
+            }
+            else if (ids[2].includes("Arm")) {
+
+                const zone = parseInt(ids[2].substring(7));
+                await Arm(zone);
+                bHandled = true;
+            }
+
+
+            if (!bHandled) {
+                adapter.log.debug("### not handled " + id + " " + JSON.stringify(state));
+            }
+            else {
+                adapter.log.debug("### all StateChange handled ");
+            }
+
+        }
+        else {
+            adapter.log.debug("### state change already handled: " + LastStateVal + " / " + state.val + " /// " + id + " / " + LastStateChangeID);
+        }
+    }
+    catch (e) {
+        adapter.log.error("exception in HandleStateChange [" + e + "]");
+    }
+}
+
+async function Disarm() {
+
+    adapter.log.info("all zones disarmed ");
+
+    await adapter.setStateAsync("State", { ack: true, val: "disarmed" });
+
+    UnsubscribeSensors();
+}
+
+
+function UnsubscribeSensors() {
+
+    if (ArmTimer != null) {
+        clearTimeout(ArmTimer);
+        ArmTimer = null;
+    }
+
+    for (let i = 0; i < adapter.config.sensors.length; i++) {
+        adapter.subscribeStates(adapter.config.sensors[i].OID);
+        adapter.log.debug("unsubscribe " + JSON.stringify(adapter.config.sensors[i]));
+    }
+
+}
+
+/**
+ * @param { number} zone
+ */
+async function Arm(zone) {
+
+    if (adapter.config.DelayBeforeArm > 0) {
+        await adapter.setStateAsync("State", { ack: true, val: "arming" });
+        ArmTimer = setTimeout(Arm2, adapter.config.DelayBeforeArm*1000,zone);
+
+    }
+    else {
+        await Arm2(zone);
+    }
+}
+
+/**
+ * @param { number} zone
+ */
+async function Arm2(zone) {
+
+    ArmTimer = null;
+
+    if (zone < 0) {
+        adapter.log.info("arm all zones ");
+        await adapter.setStateAsync("State", { ack: true, val: "armed" });
+        SubscribeSensors(-1);
+    }
+    else {
+        adapter.log.info("arm zone " + zone);
+        SubscribeSensors(zone);
+    }
+
+    await adapter.setStateAsync("State", { ack: true, val: "armed" });
+}
+
+
+/**
+ * @param {number} zone
+ */
+function SubscribeSensors(zone) {
+
+    if (zone < 0) {
+        for (let i = 0; i < adapter.config.sensors.length; i++) {
+            adapter.log.debug("subscribe " + JSON.stringify(adapter.config.sensors[i]));
+            adapter.subscribeStates(adapter.config.sensors[i].OID);
+        }
+    }
+    else {
+        const sensors = adapter.config.sensors.filter(d => parseInt(d.Zone) === zone);
+        
+        for (let i = 0; i < sensors.length; i++) {
+            adapter.log.debug("subscribe " + JSON.stringify(sensors[i]));
+            adapter.subscribeStates(sensors[i].OID);
+        }
+
+    }
+
+}
+
+
+// If started as allInOne/compact mode => return function to create instance
+if (module && module.parent) {
+    module.exports = startAdapter;
 } else {
-    // otherwise start the instance directly
-    new MiniAlarm();
+    // or start the instance directly
+    startAdapter();
 }
